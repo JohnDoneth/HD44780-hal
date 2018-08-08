010e7bc4f9dc7da200c1dbd09cd7090b080c34d3
John Doneth <doneth7@gmail.com>
2018-08-08 11:44:06 -0400

    Abstract away the data bus to make way for the 4 bit bus impl

--- src/lib.rs
+++ src/lib.rs
@@ -6,29 +6,193 @@ use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::blocking::delay::DelayUs;
 use embedded_hal::digital::OutputPin;
 
+pub trait DataBus {
+
+    fn write<D : DelayUs<u16> + DelayMs<u8>>(&mut self, byte : u8, data : bool, delay : &mut D);
+
+    // TODO
+    // fn read(...)
+
+}
+
+pub struct EightBitBus<
+    RS: OutputPin,
+    EN: OutputPin,
+    D0: OutputPin,
+    D1: OutputPin,
+    D2: OutputPin,
+    D3: OutputPin,
+    D4: OutputPin,
+    D5: OutputPin,
+    D6: OutputPin,
+    D7: OutputPin,
+> {
+    rs: RS,
+    en: EN,
+    d0: D0,
+    d1: D1,
+    d2: D2,
+    d3: D3,
+    d4: D4,
+    d5: D5,
+    d6: D6,
+    d7: D7,
+}
+
+impl<
+    RS: OutputPin,
+    EN: OutputPin,
+    D0: OutputPin,
+    D1: OutputPin,
+    D2: OutputPin,
+    D3: OutputPin,
+    D4: OutputPin,
+    D5: OutputPin,
+    D6: OutputPin,
+    D7: OutputPin
+> EightBitBus<RS, EN, D0, D1, D2, D3, D4, D5, D6, D7> {
+
+    fn set_bus_bits(&mut self, data: u8) {
+
+        let db0: bool = (0b0000_0001 & data) != 0;
+        let db1: bool = (0b0000_0010 & data) != 0;
+        let db2: bool = (0b0000_0100 & data) != 0;
+        let db3: bool = (0b0000_1000 & data) != 0;
+        let db4: bool = (0b0001_0000 & data) != 0;
+        let db5: bool = (0b0010_0000 & data) != 0;
+        let db6: bool = (0b0100_0000 & data) != 0;
+        let db7: bool = (0b1000_0000 & data) != 0;
+
+        if db0 {
+            self.d0.set_high();
+        } else {
+            self.d0.set_low();
+        }
+
+        if db1 {
+            self.d1.set_high();
+        } else {
+            self.d1.set_low();
+        }
+
+        if db2 {
+            self.d2.set_high();
+        } else {
+            self.d2.set_low();
+        }
+
+        if db3 {
+            self.d3.set_high();
+        } else {
+            self.d3.set_low();
+        }
+
+        if db4 {
+            self.d4.set_high();
+        } else {
+            self.d4.set_low();
+        }
+
+        if db5 {
+            self.d5.set_high();
+        } else {
+            self.d5.set_low();
+        }
+
+        if db6 {
+            self.d6.set_high();
+        } else {
+            self.d6.set_low();
+        }
+
+        if db7 {
+            self.d7.set_high();
+        } else {
+            self.d7.set_low();
+        }
+    }
+
+}
+
+impl<
+    RS: OutputPin,
+    EN: OutputPin,
+    D0: OutputPin,
+    D1: OutputPin,
+    D2: OutputPin,
+    D3: OutputPin,
+    D4: OutputPin,
+    D5: OutputPin,
+    D6: OutputPin,
+    D7: OutputPin,
+> DataBus for EightBitBus<RS, EN, D0, D1, D2, D3, D4, D5, D6, D7> {
+
+    fn write<D : DelayUs<u16> + DelayMs<u8>>(&mut self, byte : u8, data : bool, delay : &mut D) {
+        
+        if data == true {
+            self.rs.set_high();
+        } else {
+            self.rs.set_low();
+        }
+
+        self.set_bus_bits(byte);
+
+        self.en.set_high();
+        delay.delay_ms(15u8);
+        self.en.set_low();
+
+        if data == true {
+            self.rs.set_low();
+        }
+
+    }
+
+}
+
+pub struct FourBitBus<
+    RS: OutputPin,
+    EN: OutputPin,
+    D4: OutputPin,
+    D5: OutputPin,
+    D6: OutputPin,
+    D7: OutputPin
+> {
+    rs: RS,
+    en: EN,
+    d4: D4,
+    d5: D5,
+    d6: D6,
+    d7: D7,
+}
+
+impl<
+    RS: OutputPin,
+    EN: OutputPin,
+    D4: OutputPin,
+    D5: OutputPin,
+    D6: OutputPin,
+    D7: OutputPin,
+> DataBus for FourBitBus<RS, EN, D4, D5, D6, D7> {
+
+    fn write<D : DelayUs<u16> + DelayMs<u8>>(&mut self, byte : u8, data : bool, delay : &mut D) {
+        
+        unimplemented!("Not yet implemented");
+
+        //self.set_bus_bits(byte);
+
+        // self.en.set_high();
+        // delay.delay_ms(15u8);
+        // self.en.set_low();
+
+    }
+
+}
+
 pub struct HD44780<
     D: DelayUs<u16> + DelayMs<u8>,
-    O1: OutputPin,
-    O2: OutputPin,
-    O3: OutputPin,
-    O4: OutputPin,
-    O5: OutputPin,
-    O6: OutputPin,
-    O7: OutputPin,
-    O8: OutputPin,
-    O9: OutputPin,
-    O10: OutputPin,
+    B: DataBus,
 > {
-    rs: O1,
-    en: O2,
-    db0: O3,
-    db1: O4,
-    db2: O5,
-    db3: O6,
-    db4: O7,
-    db5: O8,
-    db6: O9,
-    db7: O10,
+    bus : B,
     delay: D,
 }
 
@@ -38,19 +202,17 @@ pub enum Direction {
     Right,
 }
 
-impl<
-        D: DelayUs<u16> + DelayMs<u8>,
-        O1: OutputPin,
-        O2: OutputPin,
-        O3: OutputPin,
-        O4: OutputPin,
-        O5: OutputPin,
-        O6: OutputPin,
-        O7: OutputPin,
-        O8: OutputPin,
-        O9: OutputPin,
-        O10: OutputPin,
-    > HD44780<D, O1, O2, O3, O4, O5, O6, O7, O8, O9, O10>
+impl<D: DelayUs<u16> + DelayMs<u8>,
+    RS: OutputPin,
+    EN: OutputPin,
+    D0: OutputPin,
+    D1: OutputPin,
+    D2: OutputPin,
+    D3: OutputPin,
+    D4: OutputPin,
+    D5: OutputPin,
+    D6: OutputPin,
+    D7: OutputPin> HD44780<D, EightBitBus<RS, EN, D0, D1, D2, D3, D4, D5, D6, D7>>
 {
     /// Create an instance of a `HD44780` from 8 data pins, a register select
     /// pin, an enable pin and a struct implementing the delay trait.
@@ -64,30 +226,32 @@ impl<
     /// is data on the 8 data pins and that it should read them in.
     /// 
     pub fn new_8bit(
-        rs: O1,
-        en: O2,
-        db0: O3,
-        db1: O4,
-        db2: O5,
-        db3: O6,
-        db4: O7,
-        db5: O8,
-        db6: O9,
-        db7: O10,
+        rs: RS,
+        en: EN,
+        d0: D0,
+        d1: D1,
+        d2: D2,
+        d3: D3,
+        d4: D4,
+        d5: D5,
+        d6: D6,
+        d7: D7,
         delay: D,
-    ) -> HD44780<D, O1, O2, O3, O4, O5, O6, O7, O8, O9, O10> {
+    ) -> HD44780<D, EightBitBus<RS, EN, D0, D1, D2, D3, D4, D5, D6, D7>> {
         
         let mut hd = HD44780 {
-            rs,
-            en,
-            db0,
-            db1,
-            db2,
-            db3,
-            db4,
-            db5,
-            db6,
-            db7,
+            bus : EightBitBus {
+                rs,
+                en,
+                d0,
+                d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+            },
             delay,
         };
 
@@ -96,13 +260,79 @@ impl<
         return hd;
     }
 
+}
+
+impl<D: DelayUs<u16> + DelayMs<u8>, 
+    RS: OutputPin,
+    EN: OutputPin,
+    D4: OutputPin,
+    D5: OutputPin,
+    D6: OutputPin,
+    D7: OutputPin> HD44780<D, FourBitBus<RS, EN, D4, D5, D6, D7>>
+{
+
+    /// Create an instance of a `HD44780` from 4 data pins, a register select
+    /// pin, an enable pin and a struct implementing the delay trait.
+    /// - The delay instance is used to sleep between commands to 
+    /// ensure the `HD44780` has enough time to process commands.
+    /// - The four db0..db3 pins are used to send and recieve with
+    ///  the `HD44780`.
+    /// - The register select pin is used to tell the `HD44780` 
+    /// if incoming data is a command or data.
+    /// - The enable pin is used to tell the `HD44780` that there 
+    /// is data on the 4 data pins and that it should read them in.
+    /// 
+    /// This mode operates differently than 8 bit mode by using 4 less
+    /// pins for data, which is nice on devices with less I/O
+    ///
+    /// Instead of commands being sent byte by byte each command is
+    /// broken up into it's upper and lower nibbles (4 bits) before
+    /// being sent over the data bus
+    ///
+    pub fn new_4bit<
+        
+    >(
+        rs: RS,
+        en: EN,
+        d4: D4,
+        d5: D5,
+        d6: D6,
+        d7: D7,
+        delay: D,
+    ) -> HD44780<D, FourBitBus<RS, EN, D4, D5, D6, D7>> {
+        
+        let mut hd = HD44780 {
+            bus : FourBitBus {
+                rs,
+                en,
+                d4,
+                d5,
+                d6,
+                d7,
+            },
+            delay,
+        };
+
+        //hd.init_8bit();
+
+        return hd;
+    }
+
+}
+
+impl<D, B> HD44780<D, B> where 
+    D: DelayUs<u16> + DelayMs<u8>, 
+    B: DataBus,
+{
+
     /// Unshifts the display and sets the cursor position to 0
     /// 
     /// ```
     /// lcd.reset();
     /// ```
     pub fn reset(&mut self) {
-        self.send_byte(0b0000_0010);
+
+        self.bus.write(0b0000_0010, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(50);
@@ -141,7 +371,7 @@ impl<
 
         let cmd_byte = 0b0000_1000 | display_bit | cursor_visible_bit | cursor_blink_bit;
 
-        self.send_byte(cmd_byte);
+        self.bus.write(cmd_byte, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(50);
@@ -153,7 +383,8 @@ impl<
     /// lcd.clear();
     /// ```
     pub fn clear(&mut self) {
-        self.send_byte(0b0000_0001);
+
+        self.bus.write(0b0000_0001, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(50);
@@ -169,7 +400,7 @@ impl<
         
         let lower_7_bits = 0b0111_1111 & position;
         
-        self.send_byte(0b1000_0000 | lower_7_bits);
+        self.bus.write(0b1000_0000 | lower_7_bits, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(50);
@@ -187,7 +418,7 @@ impl<
             Direction::Right => 0b0000_0100,
         };
 
-        self.send_byte(0b0001_0000 | bits);
+        self.bus.write(0b0001_0000 | bits, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(50);
@@ -205,7 +436,7 @@ impl<
             Direction::Right => 0b0000_0100,
         };
 
-        self.send_byte(0b0001_1000 | bits);
+        self.bus.write(0b0001_1000 | bits, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(50);
@@ -217,36 +448,36 @@ impl<
         self.delay.delay_ms(15u8);
 
         // Initialize Lcd in 8-bit mode
-        self.send_byte(0b0011_0000);
+        self.bus.write(0b0011_0000, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_ms(5u8);
 
         // Enable 5x7 mode for chars
-        self.send_byte(0b0011_1000);
+        self.bus.write(0b0011_1000, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(100);
 
-        self.send_byte(0b0000_1110);
+        self.bus.write(0b0000_1110, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(100);
 
         // Clear Display
-        self.send_byte(0b0000_0001);
+        self.bus.write(0b0000_0001, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(100);
 
         // Move the cursor to beginning of first line
-        self.send_byte(0b000_0111);
+        self.bus.write(0b000_0111, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(100);
 
         // Set entry mode to increment by one
-        self.send_byte(0b000_0110);
+        self.bus.write(0b000_0110, false, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(100);
@@ -269,11 +500,8 @@ impl<
     /// lcd.write_char('A');
     /// ```
     pub fn write_char(&mut self, data: char) {
-        self.rs.set_high();
 
-        self.send_byte(data as u8);
-
-        self.rs.set_low();
+        self.bus.write(data as u8, true, &mut self.delay);
 
         // Wait for the command to be processed
         self.delay.delay_us(100);
@@ -281,76 +509,17 @@ impl<
 
     // Send a byte to the HD44780 by setting the data on the bus and
     // also pulsing the enable pin
-    fn send_byte(&mut self, data: u8) {
+    /*fn send_byte(&mut self, data: u8) {
         // Pulse the enable pin
         self.set_bus_bits(data);
         self.pulse_enable();
-    }
+    }*/
 
     // Pulse the enable pin telling the HD44780 that we something for it
-    fn pulse_enable(&mut self) {
+    /*fn pulse_enable(&mut self) {
         self.en.set_high();
         self.delay.delay_ms(15u8);
         self.en.set_low();
-    }
+    }*/
 
-    // Set the pins on the data bus
-    fn set_bus_bits(&mut self, data: u8) {
-        let db0: bool = (0b0000_0001 & data) != 0;
-        let db1: bool = (0b0000_0010 & data) != 0;
-        let db2: bool = (0b0000_0100 & data) != 0;
-        let db3: bool = (0b0000_1000 & data) != 0;
-        let db4: bool = (0b0001_0000 & data) != 0;
-        let db5: bool = (0b0010_0000 & data) != 0;
-        let db6: bool = (0b0100_0000 & data) != 0;
-        let db7: bool = (0b1000_0000 & data) != 0;
-
-        if db0 {
-            self.db0.set_high();
-        } else {
-            self.db0.set_low();
-        }
-
-        if db1 {
-            self.db1.set_high();
-        } else {
-            self.db1.set_low();
-        }
-
-        if db2 {
-            self.db2.set_high();
-        } else {
-            self.db2.set_low();
-        }
-
-        if db3 {
-            self.db3.set_high();
-        } else {
-            self.db3.set_low();
-        }
-
-        if db4 {
-            self.db4.set_high();
-        } else {
-            self.db4.set_low();
-        }
-
-        if db5 {
-            self.db5.set_high();
-        } else {
-            self.db5.set_low();
-        }
-
-        if db6 {
-            self.db6.set_high();
-        } else {
-            self.db6.set_low();
-        }
-
-        if db7 {
-            self.db7.set_high();
-        } else {
-            self.db7.set_low();
-        }
-    }
 }
